0970d838931bdaa055908ba398152d68e8ee0424
Shreyas Devalapurkar <shdevalapurkar@hotmail.com>
2017-05-11 09:57:05 -0500

    created data model classes for message topics and their replies

---
+++ MessageBoard/Data/Reply.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MessageBoard.Data
+{
+    public class Reply
+    {
+        public string Body { get; set; }
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+
+        public int TopicId { get; set; }
+    }
+}
